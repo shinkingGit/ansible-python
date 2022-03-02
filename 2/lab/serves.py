a5b42cd261f67d71e400750a0319d6675fa10ed5
shinkingGit <pinky_91907991@icloud.com>
2022-03-02 02:51:24 +0000

    add

--- 2/lab/serves.py
+++ 2/lab/serves.py
@@ -1,19 +1,20 @@
 from flask import Flask, make_response ,request
 from waitress import serve
+import subprocess
 
 api = Flask(__name__)
 
 @api.route('/create', methods=['GET'])
-def get():
-    name = request.args.get('name')
-    print('createからアクセスがありました。')
-    return make_response('yamlファイルを作成しました')
+def create():
+    subprocess.run(['python3','convertE2Y'],shell=False)
+    print('yamlファイルを作成しました')
+    return make_response('success')
 
 @api.route('/ansible', methods=['GET'])
-def get():
-    name = request.args.get('name')
-    print('ansibleからアクセスがありました。')
-    return make_response('Ansibleを実行しました')
+def ansible():
+    subprocess.run(['ansible-playbook','main.yaml'],shell=False)
+    print('ansibleを実行しました')
+    return make_response('success')
 
 if __name__ == '__main__':
     serve(api, host='0.0.0.0', port=3000)
